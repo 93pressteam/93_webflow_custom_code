3951e667d57dee7e16efc8c7d0982c474a8a041d
Ninety Three <184813906+93pressteam@users.noreply.github.com>
2024-10-17 11:13:52 +0300

    final test

--- src/js/pages/support.js
+++ src/js/pages/support.js
@@ -1,5 +1,7 @@
 export function initSupportPage() {
     console.log("Support page scripts loaded");
+    console.log('fanal test');
+    
     
     // Додайте специфічну логіку для сторінки Support
 }
